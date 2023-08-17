42a7d4aa4b4a7c8d827761f0f68768a650c58be4
rodrigo <olivertate274@gmail.com>
2023-08-16 20:09:21 -0600

    creation of Numbers component and using DRY Dont repeat yourself

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -1,49 +1,30 @@
 //Import
 import React              from 'react'
 import { Result }         from './components/Result'
-import { Button }         from './components/Button'
 import { MathOperations } from './components/MathOperations'
-import { Functions }   from './components/Functions'
+import { Functions }      from './components/Functions'
+import { Numbers }        from './components/Numbers'
 import './App.css'
 
 // Generar
 const App = () => {
   console.log( 'render' )
 
-  const onHandlerClickfunction = text => {
-    console.log( 'Text click', text )
-  }
-
   return (
     <main className='container'>
       <Result/>
-      <section className='calc'>
-        <div>
-          <div className='num'>
-            <Button type={ '_1' } text={ '1' } handlerClick={ onHandlerClickfunction } />
-            <Button type={ '_1' } text={ '2' } handlerClick={ onHandlerClickfunction } />
-            <Button type={ '_1' } text={ '3' } handlerClick={ onHandlerClickfunction } />
-          </div>
-          <div className='num'>
-            <Button type={ '_1' } text={ '4' } handlerClick={ onHandlerClickfunction } />
-            <Button type={ '_1' } text={ '5' } handlerClick={ onHandlerClickfunction } />
-            <Button type={ '_1' } text={ '6' } handlerClick={ onHandlerClickfunction } />
-          </div>
-          <div className='num'>
-            <Button type={ '_1' } text={ '7' } handlerClick={ onHandlerClickfunction } />
-            <Button type={ '_1' } text={ '8' } handlerClick={ onHandlerClickfunction } />
-            <Button type={ '_1' } text={ '9' } handlerClick={ onHandlerClickfunction } />
-          </div>
-          <div className='num'>
-            <Button type={ '_1' } text={ '0' } handlerClick={ onHandlerClickfunction } />
-          </div>
-          <Functions 
-            onClickClear={ compClear => console.log( 'ComClear', compClear )} 
-            onClickDelete={ compDelete => console.log( 'compDelete', compDelete ) } />
-        </div>
+      <section className='calc'>        
+          <Numbers onClickNumber={
+            numberText => {
+              console.log( 'numberText', numberText )
+            }
+          }/>
         <MathOperations 
         onClickOperation={ operation  => console.log( 'Operations', operation )} 
         onClickEqual={ equal => console.log( 'Equal', equal ) } />
+        <Functions 
+            onClickClear={ compClear => console.log( 'ComClear', compClear )} 
+            onClickDelete={ compDelete => console.log( 'compDelete', compDelete ) } />
       </section>
     </main>
   )
---
+++ calc-app/src/components/Numbers.jsx
@@ -0,0 +1,27 @@
+import React      from 'react'
+import PropTypes  from 'prop-types'
+import { Button } from './Button'
+
+const Numbers = ({ onClickNumber }) => {
+
+  const numbers = [1, 2, 3, 4, 5, 6 ,7 , 8, 9, 0]
+  // DRY Dont repeat yourself
+  
+  const iterateButtton = num => {
+    return <Button type={ '_1' } text={ `${ num }` } handlerClick={ onClickNumber } />
+  }
+
+  return (
+      <section className='num'>
+        {
+          numbers.map( itm => iterateButtton( itm ))
+        }
+      </section>
+  )
+}
+
+Numbers.propTypes = {
+  onClickNumber: PropTypes.func.isRequired
+}
+
+export { Numbers }
