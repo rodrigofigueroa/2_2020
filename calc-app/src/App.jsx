424e8078110129b6de18bfab0ff2b7babfedf9e5
rodrigo <olivertate274@gmail.com>
2023-08-18 21:05:48 -0600

    Adding Hooks React.useState variable and function returns those things

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -1,22 +1,29 @@
 //Import
-import React              from 'react'
-import { Result }         from './components/Result'
-import { MathOperations } from './components/MathOperations'
-import { Functions }      from './components/Functions'
-import { Numbers }        from './components/Numbers'
+import React, { useState }  from 'react'
+import { Result }           from './components/Result'
+import { MathOperations }   from './components/MathOperations'
+import { Functions }        from './components/Functions'
+import { Numbers }          from './components/Numbers'
 import './App.css'
 
 // Generar
 const App = () => {
   console.log( 'render' )
 
+  const arrayValueFunction = useState( '' )
+  // Texto
+  const texto = arrayValueFunction[ 0 ]
+  // Funcion para modificar el texto
+  const functionTexto = arrayValueFunction[ 1 ]
+
   return (
     <main className='container'>
-      <Result/>
+      <Result value={ texto }/>
       <section className='calc'>        
           <Numbers onClickNumber={
             numberText => {
               console.log( 'numberText', numberText )
+              functionTexto( numberText )
             }
           }/>
         <MathOperations 
